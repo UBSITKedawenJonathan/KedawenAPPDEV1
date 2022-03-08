061f4d9a893c61f1f275776ab5b68138135d5d4c
UBSITKedawenJonathan <97932183+UBSITKedawenJonathan@users.noreply.github.com>
2022-03-08 14:12:22 +0800

    Update app component

--- Midterms/appdev-samp/src/app/app.component.ts
+++ Midterms/appdev-samp/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
+declare var require:any
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  /*template: `<h1>Hello World</h1>
+  <p>Multiline HTML Documentation</p>`,
+*/
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'appdev-samp';
+  title = 'Kedawen-APPDEV1-SAMP';
 }
--- Midterms/appdev-samp/src/app/app.module.ts
+++ Midterms/appdev-samp/src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CourseListComponent } from './course-list/course-list.component';
+import { NaviComponent } from './navi/navi.component';
+import { CourseInfoComponent } from './course-info/course-info.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { ContactInfoComponent } from './contact-info/contact-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseListComponent,
+    NaviComponent,
+    CourseInfoComponent,
+    ArticlesComponent,
+    ContactInfoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
