02846e0468f6d9fd1bf37591d0bd02b50e3af9f0
UBSITKedawenJonathan <97932183+UBSITKedawenJonathan@users.noreply.github.com>
2022-03-17 14:40:50 +0800

    APPDEV MIDTERM  EXER 3
    MAAM SA GITHUB PO AKO MISMO NAG COMIT KASI MAY PROBLEMS PO ATA SA VSCODE

--- Midterms/appdev-samp/src/app/course-info/course-info.component.ts
+++ Midterms/appdev-samp/src/app/course-info/course-info.component.ts
@@ -1,12 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-
+export interface Student{
+  idnumber:string;
+  name:string;
+  dateadded:Date;
+  course:string;
+}
 @Component({
   selector: 'app-course-info',
   templateUrl: './course-info.component.html',
   styleUrls: ['./course-info.component.css']
 })
-export class CourseInfoComponent implements OnInit {
 
+export class CourseInfoComponent implements OnInit {
+  student: Student = {
+    idnumber: '20193060',
+    name: 'jonathan',
+    dateadded: new Date(2010,12,16),
+    course:'BSCS'
+  };
+  student1: Student = {
+    idnumber: '123',
+    name: 'John',
+    dateadded: new Date(2017,11,13),
+    course:'BSIT'
+  };
+  student2: Student = {
+    idnumber: '5123',
+    name: 'Abraham',
+    dateadded: new Date(2018,10,14),
+    course:'BSCS'
+  };
   constructor() { }
 
   ngOnInit(): void {
